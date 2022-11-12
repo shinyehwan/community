d3b348f5fa3a22791086f993ced623ec549b2f05
shinyehwan <jghisjl007@naver.com>
2022-11-13 01:23:13 +0900

    게시물리스트에 링크, 게시물 상세페이지

--- src/main/java/com/ll/exam/article/ArticleController.java
+++ src/main/java/com/ll/exam/article/ArticleController.java
@@ -31,4 +31,14 @@ public class ArticleController {
 		rq.appendBody("%d번 게시물이 생성되었습니다.".formatted(id));
 
 	}
+
+	public void showDetail(Rq rq) {
+
+		long id = 1;
+
+		ArticleDto articleDto = articleService.findById(id);
+
+		rq.setAttr("article", articleDto);
+		rq.view("usr/article/detail");
+	}
 }
